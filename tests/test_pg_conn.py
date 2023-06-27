f053ad199f3fcebedd6b193e4cf3e355ee31f296
Prathamesh Ghatole <prathamesh.s.ghatole@gmail.com>
2023-06-27 21:36:58 +0530

    improved test_pg_conn.py with better readability and assertion messages

--- tests/test_pg_conn.py
+++ tests/test_pg_conn.py
@@ -1,16 +1,28 @@
 import psycopg2 as pg
 from musicbrainz_bot import config
 
-URI = config.MB_TEST_DB
 
+def make_connection(use_test_db: bool) -> bool:
+    if use_test_db is True:
+        URI = config.MB_TEST_DB
+    else:
+        URI = config.MB_DB
 
-def test_connection():
-    is_success: bool = False
     try:
         _ = pg.connect(URI)
-        is_success = True
-        assert is_success is True
+        return True
 
-    except Exception as e:
-        print("PostgreSQL TEST Connection Failed")
-        print(e)
+    except Exception:
+        return False
+
+
+def test_TEST_DB_connection() -> None:
+    """Test the connection to the test database."""
+    result = make_connection(use_test_db=True)
+    assert result is True, "Connection to the TEST database failed."
+
+
+def test_DB_connection() -> None:
+    """Test the connection to the main database."""
+    result = make_connection(use_test_db=False)
+    assert result is True, "Connection to the main database failed."
