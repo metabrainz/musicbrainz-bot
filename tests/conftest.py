f294be09dfca069b33209ef66ff9ea6786aa0c07
Prathamesh Ghatole <prathamesh.s.ghatole@gmail.com>
2023-08-11 18:32:01 +0530

    updated conftest and utils to now use a single database connection.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,19 @@
 import pytest
 import tests.utils as utils
+import psycopg2 as pg
+from musicbrainz_bot import config as cfg
 
 
 @pytest.fixture(scope="session", autouse=True)
-def reset_db():
-    return utils.reset_db()
+def reset_db(db_conn):
+    return utils.reset_db(db_conn)
+
+
+@pytest.fixture(scope="session")
+def db_conn():
+    conn = pg.connect(cfg.MB_TEST_DB)
+    conn.autocommit = True
+
+    yield conn
+
+    conn.close()
--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,3 @@
-import psycopg as pg
 import musicbrainz_bot.config as cfg
 from os import system as system
 from time import perf_counter
@@ -8,26 +7,28 @@ Defines some utility functions for testing on the musicbrainz_test database.
 """
 
 
-def _get_free_editor_id():
+def _get_free_editor_id(db_conn):
     """
     Returns the lowest editor ID that is not currently in use.
     """
-    with pg.connect(cfg.MB_TEST_DB) as conn:
-        cur = conn.cursor()
-        query = """
-        SELECT id FROM editor
-        ORDER BY id DESC
-        LIMIT 1;"""
-        cur.execute(query)
-        results = cur.fetchone()
+    cur = db_conn.cursor()
+    query = """
+    SELECT id FROM editor
+    ORDER BY id DESC
+    LIMIT 1;"""
+    cur.execute(query)
+    results = cur.fetchone()
 
-        if results is None:
-            return 1
-        else:
-            return results[0] + 2
+    cur.close()
+
+    if results is None:
+        return 1
+    else:
+        return results[0] + 2
 
 
 def create_user(
+    db_conn,
     username: str = "test_user",
     password: str = "pass",
     privs: int = 256,
@@ -38,49 +39,52 @@ def create_user(
     Creates a user on the database with the given username, password, and privileges.
     """
     password = "{CLEARTEXT}pass"
-    db_URI = cfg.MB_TEST_DB if use_test_db else cfg.MB_DB
+    # db_URI = cfg.MB_TEST_DB if use_test_db else cfg.MB_DB
 
-    with pg.connect(db_URI) as conn:
-        cur = conn.cursor()
-        query = (
-            "INSERT INTO editor "
-            "(id, name, password, privs, email, website, bio, email_confirm_date, member_since, last_login_date, ha1) "
-            "VALUES (%s, %s, %s, %s, 'test@editor.org', 'http://musicbrainz.org', 'biography', '2005-10-20', '1989-07-23', now(), 'e1dd8fee8ee728b0ddc8027d3a3db478');"
-        )
-        values = (id, username, password, privs)
-        cur.execute(query, values)
+    # with pg.connect(db_URI) as conn:
+    cur = db_conn.cursor()
+    query = (
+        "INSERT INTO editor "
+        "(id, name, password, privs, email, website, bio, email_confirm_date, member_since, last_login_date, ha1) "
+        "VALUES (%s, %s, %s, %s, 'test@editor.org', 'http://musicbrainz.org', 'biography', '2005-10-20', '1989-07-23', now(), 'e1dd8fee8ee728b0ddc8027d3a3db478');"
+    )
+    values = (id, username, password, privs)
+    cur.execute(query, values)
+
+    cur.close()
 
     return id
 
 
 def delete_user(
+    db_conn,
     id: int,
-    db_URI: str = cfg.MB_TEST_DB,
 ) -> int:
     """
     Deletes a user on the database with the given id.
     """
-    with pg.connect(db_URI) as conn:
-        cur = conn.cursor()
-        query = "DELETE FROM editor WHERE id = %s;"
-        values = (id,)
-        cur.execute(query, values)
+    cur = db_conn.cursor()
+    query = "DELETE FROM editor WHERE id = %s;"
+    values = (id,)
+    cur.execute(query, values)
+
+    cur.close()
 
     return id
 
 
-def reset_db():
+def reset_db(db_conn):
     try:
         start = perf_counter()
         system(
             f'sudo docker exec {cfg.MUSICBRAINZ_CONTAINER_ID} "script/create_test_db.sh"'
         )
-        create_user()
+        create_user(db_conn)
 
         end = perf_counter()
 
         print(f"Database reset in {round(end - start, 2)} seconds")
         return True
 
-    except Exception:
-        raise Exception("Failed to reset database")
+    except Exception as e:
+        raise Exception(f"Failed to reset database: {e}")
