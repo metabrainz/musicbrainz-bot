5e4a8b19b0e26d731e7dbc78028b7f7d94b33039
Prathamesh Ghatole <prathamesh.s.ghatole@gmail.com>
2023-09-11 23:18:58 +0530

    refactored scripts/reset_test_db.sh to remove excessive comments and permission requirements.

--- scripts/reset_test_db.sh
+++ scripts/reset_test_db.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-# Make sure to give this script execution permission (chmod +x reset_test_db.sh)
 
-# Check if the script is run as root (sudo)
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script with sudo."
-  exit 1
-fi
-
-# Define the container name
 container_name="musicbrainz-docker-musicbrainz-1"
 
-# Define the command to be executed
 command="script/create_test_db.sh"
 
-# Run the docker exec command
 docker exec "$container_name" "$command"
